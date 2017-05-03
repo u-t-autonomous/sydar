6b4511abc615a803096ad46c25987f7ec12cf1ee
sahabi <sahabi@gmail.com>
2017-05-02 21:31:20 -0500

    fixed problem with versioning

--- setup.py
+++ setup.py
@@ -1,28 +1,23 @@
 from setuptools import setup
-
+from sydar.version import __version__
 def readme():
     with open('README.rst') as f:
         return f.read()
 
-def version():
+def version():   
     try:
-        with open('../version.txt') as f:
-            return f.read()
-    except:
-        pass       
-    try:
-        with open('version.txt') as f:
+        with open('VERSION') as f:
             return f.read() 
     except:
         pass
     try:
-        with open('sydar/version.txt') as f:
+        with open('sydar/versi') as f:
             return f.read() 
     except:
         pass
 
 setup(name='sydar',
-      version=version(),
+      version=__version__,
       description='Synthesis Done Approximately Right',
       long_description=readme(),
       url='https://github.com/u-t-autonomous/sydar.git',
